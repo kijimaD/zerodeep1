30aaf8c87277e1c423cf3b957ffffb55530cded3
Kijima Daigo <norimaking777@gmail.com>
2024-07-11 14:26:01 +0900

    feat: use neuralnet

--- ch03/mnist_test.py
+++ ch03/mnist_test.py
@@ -2,6 +2,7 @@ import sys, os
 sys.path.append(os.pardir)
 from dataset.mnist import load_mnist
 
+# (訓練画像、訓練ラベル), (テスト画像、テストラベル)
 (x_train, t_train), (x_train, t_test) = \
     load_mnist(flatten=True, normalize=False)
 
